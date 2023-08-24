99f00c1f0b7c4e65add87ba58ad95011b1a6a725
aubyn <aubyn@163.com>
2023-08-24 18:15:51 +0800

    增删改操作

--- bookmanager02/book/models.py
+++ bookmanager02/book/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
+
 # Create your models here.
+
+class BookInfo(models.Model):
+    name = models.CharField(max_length=20, unique=True, verbose_name='名字')
+    pub_date = models.DateField(null=True)
+    readcount = models.IntegerField(default=0)
+    commentcount = models.IntegerField(default=0)
+    is_delete = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'bookinfo'
+        verbose_name = '书籍管理'
+
+    def __str__(self):
+        return self.name
+
+
+class PeopleInfo(models.Model):
+    # 定义一个有序字典
+    GENDER_CHOICE = (
+        (1, 'male'),
+        (2, 'female')
+    )
+
+    name = models.CharField(max_length=10, unique=True)
+    gender = models.SmallIntegerField(choices=GENDER_CHOICE, default=1)
+    description = models.CharField(max_length=100, null=True)
+    is_delete = models.BooleanField(default=False)
+    # 外键
+    # 系统会自动为外键添加 _id  外键的级联操作 主表和从表
+    book = models.ForeignKey(BookInfo, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'peopleinfo'
+        verbose_name = '人物管理'
+
+    def __str__(self):
+        return self.name
--- bookmanager02/book/views.py
+++ bookmanager02/book/views.py
@@ -5,4 +5,32 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
+    # 在这里实现 增删改查
+
     return HttpResponse('index')
+
+########################新增数据############################3
+from book.models import BookInfo
+# 方法1
+book = BookInfo(
+    name='Django',
+    pub_date='2000-1-1',
+    readcount=10
+)
+# 方法2
+BookInfo.objects.create(
+    name='Flask',
+    pub_date='2001-1-1',
+    readcount=12
+)
+
+#################修改数据#######################
+# 1.方法1
+book = BookInfo.objects.get(id=6)
+book.name = 'Javascript'
+book.save()
+
+# 2.方法2
+BookInfo.objects.filter(id=6).update(name='Ruby', commentcount=66)
+
+
